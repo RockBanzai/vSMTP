f61a23db18668b92055c2966309642721e4fd2b3
LangArthur <46418814+LangArthur@users.noreply.github.com>
2023-09-05 15:31:18 +0200

    fix: remove trailing spaces in license header (#1331)

--- crates/plugins/vsmtp-plugin-dnsxl/src/api.rs
+++ crates/plugins/vsmtp-plugin-dnsxl/src/api.rs
@@ -2,14 +2,12 @@
  * vSMTP mail transfer agent
  *
  * Copyright (C) 2003 - viridIT SAS
- * Licensed under the Elastic License 2.0 
+ * Licensed under the Elastic License 2.0
  *
- * You should have received a copy of the Elastic License 2.0 along with 
+ * You should have received a copy of the Elastic License 2.0 along with
  * this program. If not, see https://www.elastic.co/licensing/elastic-license.
  *
  */
-
-
 
 use std::str::FromStr;
 
